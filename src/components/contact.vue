65bf2945db00608fd7eddf2a5cbff08c1d969ffa
Cristian Quezada Montero <Cristian Quezada@minpre.local>
2022-03-29 13:37:12 -0400

    add formpress.io to form contact

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -32,74 +32,66 @@ export default {
           </p>
 
           <div>
-            <form method="post" name="myForm" onsubmit="return validateForm()">
-              <p id="error-msg"></p>
-              <div id="simple-msg"></div>
-              <div class="row">
-                <div class="col-lg-6">
-                  <div class="mb-4">
-                    <label for="name" class="text-muted form-label">Name</label>
-                    <input
-                      name="name"
-                      id="name"
-                      type="text"
-                      class="form-control"
-                      placeholder="Enter name*"
-                    />
-                  </div>
-                </div>
-                <div class="col-lg-6">
-                  <div class="mb-4">
-                    <label for="email" class="text-muted form-label"
-                      >Email</label
-                    >
-                    <input
-                      name="email"
-                      id="email"
-                      type="email"
-                      class="form-control"
-                      placeholder="Enter email*"
-                    />
-                  </div>
-                </div>
-                <div class="col-md-12">
-                  <div class="mb-4">
-                    <label for="subject" class="text-muted form-label"
-                      >Subject</label
-                    >
-                    <input
-                      type="text"
-                      class="form-control"
-                      id="subject"
-                      name="subject"
-                      placeholder="Enter Subject.."
-                    />
-                  </div>
 
+            <form action="https://formspree.io/f/moqrnwyb" method="POST">
+            <div class="row">
+              <div class="col-lg-6">
+                  <div class="mb-4">
+                    <label for="full-name" class="text-muted form-label">Full Name</label>
+                    <input type="text" 
+                    class="form-control"
+                    name="name" 
+                    id="full-name" 
+                    placeholder="First and Last Name" 
+                    required="">
+                  </div>
+                </div>
+                <div class="col-lg-6">
+                  <div class="mb-4">
+                    <label for="email-address" class="text-muted form-label">Email Address</label>
+                    <input 
+                    type="email"
+                    class="form-control"
+                    name="_replyto" 
+                    id="email-address" 
+                    placeholder="email@domain.com" 
+                    required="">
+                  </div>
+                </div>
+                <div class="col-lg-12">
+                  <div class="mb-4">
+                    <label for="email-subject" class="text-muted form-label">Subject</label>
+                    <input 
+                    type="text"
+                    class="form-control"
+                    name="_subject" 
+                    id="email-subject" 
+                    placeholder="Enter Subject.." 
+                    required="">
+                  </div>
                   <div class="mb-4 pb-2">
-                    <label for="comments" class="text-muted form-label"
-                      >Message</label
-                    >
+                    <label for="message" class="text-muted form-label">Message</label>
                     <textarea
-                      name="comments"
-                      id="comments"
+                      name="message"
+                      id="message"
                       rows="4"
                       class="form-control"
                       placeholder="Enter message..."
+                      required=""
                     ></textarea>
                   </div>
-
-                  <button
-                    type="button"
-                    id="submit"
-                    name="send"
-                    class="btn btn-primary"
-                  >
-                    Send Message
-                  </button>
                 </div>
-              </div>
+            </div>
+            <button
+              type="submit"
+              id="submit"
+              name="send"
+              class="btn btn-primary"
+            >
+              Send Message
+            </button>
             </form>
+
           </div>
         </div>
         <!-- end col -->
